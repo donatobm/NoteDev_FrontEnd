6c9ad8ae05713e51257b7c80cc49f37a80b3b438
donatobm <93447434+donatobm@users.noreply.github.com>
2024-02-04 18:18:38 -0400

    chore: update to the way that the cards shows

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,34 +1,40 @@
 import React from "react";
-import { SafeAreaView,View,FlatList,StyleSheet,Text,StatusBar, TouchableOpacity, TouchableWithoutFeedback} from "react-native";
-import Collapsible from 'react-native-collapsible';
-import COLORS from "../constants/colors";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
+
 import Notes from "./Notes";
 
 const DATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
-    },
-  ];
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "qujsaksdhasjdaisudhasdjaskjdhakjshdkashdasudasdjkasdasndmasndkasmndakuknzxncbzxncbmznxbc mznxbcmzxnbcmznxbcmzxn",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
 
-const Card= () => {
-
-
-    return(
-
-                <View >
-                    <Notes/>
-                </View>
-
-    );
+const Card = () => {
+  return (
+    <View>
+      {DATA.map((faq, index) => (
+        <Notes key={index.toString()} title={faq.title} details={faq.id} />
+      ))}
+    </View>
+  );
 };
 
-export default Card
+export default Card;
