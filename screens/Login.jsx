be1aba9315ae92b13e51182cce03b64915671966
donatobm <93447434+donatobm@users.noreply.github.com>
2024-02-05 02:50:23 -0400

    chore: added functionality finally

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,42 +1,171 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, SafeAreaView,TextInput,Image,TouchableOpacity } from 'react-native';
-
-import Btn from '../components/Btn';
+import * as React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import Btn from "../components/Btn";
 import COLORS from "../constants/colors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { Axios, user_endpoints } from "../constants/axios";
 
-const Login = ({ navigation }) =>{
-    return(
-        <SafeAreaView style={{ backgroundColor:COLORS.purple}}>
-            <View style={{alignItems:"center"}}>
-                <Text style={{color:COLORS.white, fontSize: 64, fontWeight: 'bold', marginVertical: 10,}}>Login</Text>
-                <View style={{backgroundColor: COLORS.white, height:700, width:460, borderTopLeftRadius:130, paddingTop:100, alignItems:'center', marginHorizontal: 22}}>
-                    <Text style={{fontSize: 40, color: COLORS.purple, fontWeight: 'bold'}}>Welcome back</Text>                    
-                    <Text style={{color: COLORS.grayish_white, fontSize: 19, fontWeight: "bold", marginBottom: 20}}>Login to your account</Text>
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-            
-                    <TextInput style={{ height:48, borderRadius: 100, color:COLORS.purple, paddingHorizontal: 10, width:'80%', backgroundColor: COLORS.grayish_white, marginVertical: 10}}
-                    placeholderTextColor={COLORS.purple} placeholder='E-mail/User Name'
-                    keyboardType={"email-address"}
-                    ></TextInput>
+  const handleSubmit = async () => {
+    const userData = {
+      email: email,
+      password: password,
+    };
+    if (email === "" || password === "") {
+      Alert.alert("error", "Please fill all the fields", [{ text: "OK" }]);
+      return;
+    }
+    console.log(userData);
+    try {
+      const response = await Axios.post(user_endpoints.login, userData);
+      console.log(response.data);
+      console.log(response.data.token);
+      setEmail("");
+      setPassword("");
+      await AsyncStorage.setItem("token", response.data.token);
+      navigation.navigate("Home");
+    } catch (error) {
+      console.log(error);
+      let errors = error.response.data.message;
+      Alert.alert("error", errors, [{ text: "OK" }]);
+      console.log(errors);
+    }
+  };
 
-                    <TextInput style={{height:48 ,borderRadius: 100, color:COLORS.purple, paddingHorizontal: 10, width:'80%', backgroundColor: COLORS.grayish_white, marginVertical: 10}}
-                    placeholderTextColor={COLORS.purple} placeholder='Password'
-                    secureTextEntry={true}
-                    ></TextInput>
-                        <View style={{alignItems: 'flex-end',width:'78%',paddingRight:16, marginBottom:200}}>
-                            <Text style={{color:COLORS.purple, fontWeight:'bold', fontSize: 16}}>Forger Password?</Text>
-                        </View>
-                    <Btn TextColor={COLORS.white} Colorbtn={COLORS.purple} textColor={COLORS.white} btnLabel="Login" Press={() => {alert("Logged In"), navigation.navigate("Home")}}/>
-                    <View style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
-                            <Text>Don´t have an account?</Text>
-                            <TouchableOpacity>
-                                <Text style={{color: COLORS.purple, fontWeight:'bold', fontSize: 16}} onPress={() => navigation.navigate("Signup")}>Signup</Text>
-                            </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={{ backgroundColor: COLORS.purple }}>
+      <View style={{ alignItems: "center" }}>
+        <Text
+          style={{
+            color: COLORS.white,
+            fontSize: 64,
+            fontWeight: "bold",
+            marginVertical: 10,
+          }}
+        >
+          Login
+        </Text>
+        <View
+          style={{
+            backgroundColor: COLORS.white,
+            height: 700,
+            width: 460,
+            borderTopLeftRadius: 130,
+            paddingTop: 100,
+            alignItems: "center",
+            marginHorizontal: 22,
+          }}
+        >
+          <Text
+            style={{ fontSize: 40, color: COLORS.purple, fontWeight: "bold" }}
+          >
+            Welcome back
+          </Text>
+          <Text
+            style={{
+              color: COLORS.grayish_white,
+              fontSize: 19,
+              fontWeight: "bold",
+              marginBottom: 20,
+            }}
+          >
+            Login to your account
+          </Text>
 
-export default Login
+          <TextInput
+            style={{
+              height: 48,
+              borderRadius: 100,
+              color: COLORS.purple,
+              paddingHorizontal: 10,
+              width: "80%",
+              backgroundColor: COLORS.grayish_white,
+              marginVertical: 10,
+            }}
+            placeholderTextColor={COLORS.purple}
+            placeholder="E-mail"
+            value={email}
+            onChangeText={(email) => setEmail(email)}
+            keyboardType={"email-address"}
+          ></TextInput>
+
+          <TextInput
+            style={{
+              height: 48,
+              borderRadius: 100,
+              color: COLORS.purple,
+              paddingHorizontal: 10,
+              width: "80%",
+              backgroundColor: COLORS.grayish_white,
+              marginVertical: 10,
+            }}
+            placeholderTextColor={COLORS.purple}
+            placeholder="Password"
+            value={password}
+            onChangeText={(password) => setPassword(password)}
+            secureTextEntry={true}
+          ></TextInput>
+          <View
+            style={{
+              alignItems: "flex-end",
+              width: "78%",
+              paddingRight: 16,
+              marginBottom: 200,
+            }}
+          >
+            <Text
+              style={{ color: COLORS.purple, fontWeight: "bold", fontSize: 16 }}
+            >
+              Forget your Password?
+            </Text>
+          </View>
+          <Btn
+            TextColor={COLORS.white}
+            Colorbtn={COLORS.purple}
+            textColor={COLORS.white}
+            btnLabel="Login"
+            Press={() => {
+              handleSubmit();
+            }}
+          />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <Text>Don´t have an account?</Text>
+            <TouchableOpacity>
+              <Text
+                style={{
+                  color: COLORS.purple,
+                  fontWeight: "bold",
+                  fontSize: 16,
+                }}
+                onPress={() => navigation.navigate("Signup")}
+              >
+                Signup
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
