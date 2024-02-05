5d3ae03347366d36b1885dcf3d79be44909bec19
donatobm <93447434+donatobm@users.noreply.github.com>
2024-02-05 17:02:21 -0400

    chore: update home screen for better scroll

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,19 +1,26 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, SafeAreaView,TextInput,Image,TouchableOpacity,ScrollView } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import Btn from '../components/Btn';
+import * as React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Btn from "../components/Btn";
 import COLORS from "../constants/colors";
-import Card from '../components/Card';
+import Card from "../components/Card";
+import Floatinbutton from "../components/Floatinbutton";
 
-
-const Home = ({ navigation }) =>{
-    return(
-        <View style={{marginTop:150}}>
-            <Card/>
-        </View>
-    )
-}
-
-
+const Home = ({ navigation }) => {
+  return (
+    <ScrollView style={{ height: "90%" }}>
+      <Card />
+    </ScrollView>
+  );
+};
 
 export default Home;
